a864c4a797d30083766408ac599f28c67d532c5b
Naushad <naushad.a1013@gmail.com>
2022-07-27 05:36:40 +0530

    out root config

--- array.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let arr1 = ["a", "b", "c"];
-let arr2 = ["a", "b", "c"];
-let arr = ["a", 1, true];
-let arr4 = ["a", 1, true];
-let arr5 = ["a", 1, true];
-let arr6 = ["a"];
--- tuple.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//in tuple we can define type of each elements
-let arr5 = ["a", 1, true];
-arr5.push(20);
