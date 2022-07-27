4e33582f47c4e67e619b548ceb4b6cd98535e7cb
Naushad <naushad.a1013@gmail.com>
2022-07-27 16:32:23 +0530

    m

--- build/main.js
+++ build/main.js
@@ -1,17 +1,1 @@
 "use strict";
-class createUser {
-    constructor(account) {
-        this.account = "";
-        this.account = account;
-        console.log(this.account);
-    }
-}
-class User extends createUser {
-    constructor(name, account) {
-        super(account);
-        this.name = "";
-        this.name = name;
-        console.log(this.name);
-    }
-}
-let u1 = new User("naushad", "humana");
