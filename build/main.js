c6603dde5fe063a4ea862bccd0974196ab6c62b7
Naushad <naushad.a1013@gmail.com>
2022-07-31 20:34:48 +0530

    genrics

--- build/main.js
+++ build/main.js
@@ -1,1 +1,10 @@
 "use strict";
+let names = [];
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('resolve string data');
+    }, 2000);
+});
+promise.then((data) => {
+    data.split('');
+});
